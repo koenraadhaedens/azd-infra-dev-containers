11c3e6e3afb5dcfcfc66f9a0db5a0621beea5ac1
Koenraad Haedens <koenraad@khd.be>
2025-02-21 12:20:35 +0100

    add2

--- postprovision.ps1
+++ postprovision.ps1
@@ -7,7 +7,3 @@ Write-Output "Resource Group: $resourceGroupName"
 Write-Output "Location: $location"
 Write-Output "To access please use browser to go to https://$containerUrl Please click advanced on the SSL warning to conitnue"
 
-
-# Set environment variable (optional)
-azd env set RESOURCE_GROUP_NAME $resourceGroupName
-azd env set RESOURCE_GROUP_NAME $containerUrl
