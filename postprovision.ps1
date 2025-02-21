74d7d29a486ef2bcc1ba352d3f86fa26fff1beea
Koenraad Haedens <koenraad@khd.be>
2025-02-21 12:20:10 +0100

    add

--- postprovision.ps1
+++ postprovision.ps1
@@ -1,14 +1,11 @@
-# scripts/postprovision.ps1
-param (
-    [string]$resourceGroupName,
-    [string]$location,
-    [string]$containerUrl
-)
+$resourceGroupName = $env:AZURE_RESOURCE_GROUP
+$containerUrl= $env:containerUrl
+$location = $env:location
 
 # Example: Output the resource group name and location
 Write-Output "Resource Group: $resourceGroupName"
 Write-Output "Location: $location"
-Write-Output "To access please use browser to go to https://:$containerUrl Please click advanced on the SSL warning to conitnue"
+Write-Output "To access please use browser to go to https://$containerUrl Please click advanced on the SSL warning to conitnue"
 
 
 # Set environment variable (optional)
