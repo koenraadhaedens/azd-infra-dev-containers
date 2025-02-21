6256f7b9055aed451b1cb2a7f81fbe073725d5fc
Koenraad Haedens <koenraad@khd.be>
2025-02-21 12:29:32 +0100

    bug fix

--- postprovision.ps1
+++ postprovision.ps1
@@ -1,4 +1,6 @@
-$location = $env:location
+
+$containerUrl= $env:containerUrl
+
 
 # Example: Output the resource group name and location
 Write-Output "To access please use browser to go to $containerUrl Please click advanced on the SSL warning to conitnue and use password you setup to login"
