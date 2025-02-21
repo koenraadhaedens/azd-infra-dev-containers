974b5227b030fb66a6f00221a046ab8aa2753acd
Koenraad Haedens <koenraad@khd.be>
2025-02-21 12:25:37 +0100

    add postmessage

--- postprovision.ps1
+++ postprovision.ps1
@@ -5,5 +5,5 @@ $location = $env:location
 # Example: Output the resource group name and location
 Write-Output "Resource Group: $resourceGroupName"
 Write-Output "Location: $location"
-Write-Output "To access please use browser to go to https://$containerUrl Please click advanced on the SSL warning to conitnue"
+Write-Output "To access please use browser to go to $containerUrl Please click advanced on the SSL warning to conitnue and use password you setup to login"
 
