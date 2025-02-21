8f3ad46ee1b3c648716732a227615d721dfb5230
Koenraad Haedens <koenraad@khd.be>
2025-02-21 12:28:24 +0100

    rem

--- postprovision.ps1
+++ postprovision.ps1
@@ -1,9 +1,5 @@
-$resourceGroupName = $env:AZURE_RESOURCE_GROUP
-$containerUrl= $env:containerUrl
 $location = $env:location
 
 # Example: Output the resource group name and location
-Write-Output "Resource Group: $resourceGroupName"
-Write-Output "Location: $location"
 Write-Output "To access please use browser to go to $containerUrl Please click advanced on the SSL warning to conitnue and use password you setup to login"
 
