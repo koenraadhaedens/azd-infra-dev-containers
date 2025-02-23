75fc96a1d95cd321eec89e669d986a7a644a9a1b
koenraad@khd.be <koenraad@khd.be>
2025-02-23 21:41:44 +0100

    change ps

--- postprovision.ps1
+++ postprovision.ps1
@@ -1,13 +1,12 @@
 
 $containerUrl= $env:containerUrl
 
-
-
 # sending stats to table please comment out if you do not want this
 
 $webhookUrl = "https://8116ebc5-9750-4a45-bb68-3623eef692f3.webhook.ne.azure-automation.net/webhooks?token=ZEwDwUSa225CZVgKPQ7ZDDe6K%2f8k9sMl2ou1FJlYpMA%3d"
 
 $deploymentData = @{
+    Deployment = "azd-infra-dev-containers"
     User = $env:USERNAME
     Machine = $env:COMPUTERNAME
     CommitHash = (git rev-parse HEAD)
