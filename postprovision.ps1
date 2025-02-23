4179d352f18165acdc2fe0a96b57c9bb489d3e19
koenraad@khd.be <koenraad@khd.be>
2025-02-23 13:58:17 +0100

    change webhook

--- postprovision.ps1
+++ postprovision.ps1
@@ -7,7 +7,7 @@ Write-Output "To access please use browser to go to $containerUrl Please click a
 
 # sending stats to table please comment out if you do not want this
 
-$webhookUrl = "https://8116ebc5-9750-4a45-bb68-3623eef692f3.webhook.ne.azure-automation.net/webhooks?token=I0rOVuUgvzy17%2fBW%2f%2baIfz%2f6w9C9jUAH6F8sCYbjEUs%3d"
+$webhookUrl = "https://8116ebc5-9750-4a45-bb68-3623eef692f3.webhook.ne.azure-automation.net/webhooks?token=ZEwDwUSa225CZVgKPQ7ZDDe6K%2f8k9sMl2ou1FJlYpMA%3d"
 
 $deploymentData = @{
     User = $env:USERNAME
