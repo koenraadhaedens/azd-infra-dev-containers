e890a48fc529fec174cffa404c3fe404e9a9b09a
Koenraad Haedens <40601026+koenraadhaedens@users.noreply.github.com>
2025-02-27 09:33:22 +0100

    Update postprovision.ps1

--- postprovision.ps1
+++ postprovision.ps1
@@ -17,4 +17,5 @@ Invoke-RestMethod -Uri $webhookUrl -Method Post -Body $deploymentData -ContentTy
 Write-Output "Stats Tracked"
 
 # Example: Output the resource group name and location
+Write-Output $environmentName
 Write-Output "To access please use browser to go to $containerUrl Please click advanced on the SSL warning to conitnue and use password you setup to login"
