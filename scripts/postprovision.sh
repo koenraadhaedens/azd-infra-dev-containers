cbb43db1af9e083019699e1810816b7566e31a86
koenraadhaedens <koenraad@khd.be>
2025-05-06 10:33:18 +0200

    post

--- scripts/postprovision.sh
+++ scripts/postprovision.sh
@@ -6,7 +6,7 @@ azd env get-values > .env
 # Get environment variables
 environmentName=$AZURE_ENV_NAME
 location=$AZURE_LOCATION
-containerUrl= $env:containerUrl
+
 
 # comment the lines below if you do not want to send stats to a table
  webhookUrl="https://8116ebc5-9750-4a45-bb68-3623eef692f3.webhook.ne.azure-automation.net/webhooks?token=ZEwDwUSa225CZVgKPQ7ZDDe6K%2f8k9sMl2ou1FJlYpMA%3d"
